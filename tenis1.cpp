d8ddd51a8a875e0a2af741ad97097bcbe878d8cd
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-12-04 19:09:29 +0300

    Add files via upload

---
+++ tenis1.cpp
@@ -0,0 +1,51 @@
+﻿#include <SFML/Graphics.hpp>
+#include "settings.h"
+#include <ctime>
+using namespace sf;
+int main()
+{
+    // Объект, который, собственно, является главным окном приложения
+    RenderWindow window(VideoMode(W_WIDTH, W_HEIGHT), W_TITLE);
+    window.setFramerateLimit(FPS);
+    srand(time(nullptr));
+    RectangleShape leftBat,rightBat;
+    leftBat.setSize(Vector2f(BAT_WIDTH,BAT_HEIGHT));
+    leftBat.setFillColor(LEFT_BAD_COLOR);
+    leftBat.setPosition(BAT_OFFSET,(W_HEIGHT - BAT_HEIGHT) / 2 );
+    rightBat.setSize(Vector2f(BAT_WIDTH,BAT_HEIGHT));
+    rightBat.setFillColor(RIGHT_BAD_COLOR);
+    rightBat.setPosition(W_WIDTH - BAT_OFFSET - BAT_WIDTH,(W_HEIGHT - BAT_HEIGHT) / 2);
+    CircleShape circle;
+    circle.setRadius(RADIUS);
+    circle.setFillColor(CIRCLE_COLOR);
+    circle.setPosition((W_WIDTH - 2 * RADIUS) / 2, (W_HEIGHT - 2 * RADIUS) / 2);
+    // Главный цикл приложения. Выполняется, пока открыто окно
+    while (window.isOpen())
+    {
+        // Обрабатываем очередь событий в цикле
+        Event event;
+        while (window.pollEvent(event))
+        {
+            // Пользователь нажал на «крестик» и хочет закрыть окно?
+            if (event.type == Event::Closed)
+                // тогда закрываем его
+                window.close();
+        }
+        //Обновление игры
+        window.clear();
+        circle.move(speed_x, speed_y);
+        if (circle.getPosition().x <= 0 || circle.getPosition().x + 2 * RADIUS >= W_WIDTH) {
+            speed_x = -speed_x;
+        }
+        if (circle.getPosition().y <= 0 || circle.getPosition().y + 2 * RADIUS >= W_HEIGHT) {
+            speed_y = -speed_y;
+        }
+        // Отрисовка обьектов или окна
+        window.draw(leftBat);
+        window.draw(rightBat);
+        window.draw(circle);
+        window.display();
+    }
+
+    return 0;
+}
